40ccf9ca57daab525aae455e78aa54f5271a4656
Marcus Hjelm <marcus@makemedia.se>
2013-09-02 15:11:10 +0200

    renamed helper function number_to_telefonnummer to telefonnummer_to_human

--- lib/telefonnummer/telefonnummer_helper.rb
+++ lib/telefonnummer/telefonnummer_helper.rb
@@ -2,18 +2,18 @@ module Telefonnummer
   module TelefonnummerHelper
     PREFIX_REGEXP = %w(11 13 16 18 19 21 23 26 31 33 35 36 40 42 44 46 54 60 63 8 90 70 72 73 76).join('|')
   
-    def number_to_telefonnummer(number)
-      return number if (number =~ /\D/) || (number.size < 5) || (number.size > 10)
+    def telefonnummer_to_human(telefonnummer)
+      return telefonnummer if (telefonnummer =~ /\D/) || (telefonnummer.size < 5) || (telefonnummer.size > 10)
       
       prefix, suffix = 
-      if number[0] == '0'
-        if number =~ /\A0(#{PREFIX_REGEXP})/
-          [Regexp.last_match(0), number[Regexp.last_match(0).size..-1]]
+      if telefonnummer[0] == '0'
+        if telefonnummer =~ /\A0(#{PREFIX_REGEXP})/
+          [Regexp.last_match(0), telefonnummer[Regexp.last_match(0).size..-1]]
         else
-          [number[0..3], number[4..-1]]
+          [telefonnummer[0..3], telefonnummer[4..-1]]
         end
       else
-        [nil, number]
+        [nil, telefonnummer]
       end
   
       # suffix format
@@ -25,7 +25,7 @@ module Telefonnummer
       0.step(suffix.size-1, 2) { |i| tmp << suffix[i..i+1] } if suffix
       suffix = tmp.join(' ')
   
-      # number format
+      # telefonnummer format
       tmp = []
       tmp << prefix if prefix
       tmp << suffix
